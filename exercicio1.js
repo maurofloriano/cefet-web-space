4754e398d4a4688267dacc54ef70cde3d2ad88fc
Mauro Floriano <maurofloriano@gmail.com>
2018-09-18 08:08:44 -0300

    js done

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,17 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+
+
+document.querySelectorAll('.botao-expandir-retrair').forEach(function(button){
+	button.addEventListener('click', function(event){
+		let p = button.closest('p');
+		if(p.classList.contains('expandido')) {
+			p.classList.remove('expandido');
+			button.innerHTML = '+';
+		}
+		else {
+			p.classList.add('expandido');
+			button.innerHTML = '-';
+		}
+	});
+});
